41e275c95814662c172d159d4d8167df5856d49d
gtwirth <42541127+gtwirth@users.noreply.github.com>
2023-05-02 15:10:13 -0400

    Multiple import and export flow based on interactive and plan to make sure the correct export data is sent and to provide embedded button in the appropriate location.

--- KinaxisIntegrationFlows/KinaxisIntegrationFlows.cs
+++ KinaxisIntegrationFlows/KinaxisIntegrationFlows.cs
@@ -13,7 +13,7 @@ using System.Windows.Forms;
 
 namespace KinaxisIntegrationFlows
 {
-    internal class KinaxisIntegrationImportFlow : IDesignAddIn, IDesignAddInGuiDetails
+    internal class KinaxisIntegrationImportFlowInteractive : IDesignAddIn, IDesignAddInGuiDetails
     {
         #region IDesignAddIn Members
 
@@ -82,7 +82,7 @@ namespace KinaxisIntegrationFlows
         #endregion
     }
 
-    internal class KinaxisIntegrationFlowInteractive : IDesignAddIn, IDesignAddInGuiDetails
+    internal class KinaxisIntegrationExportFlowInteractive : IDesignAddIn, IDesignAddInGuiDetails
     {
         #region IDesignAddIn Members
 
@@ -91,7 +91,7 @@ namespace KinaxisIntegrationFlows
         /// </summary>
         public string Name
         {
-            get { return "Export Flow Interactive"; }
+            get { return "Export Flow"; }
         }
 
         /// <summary>
@@ -141,6 +141,139 @@ namespace KinaxisIntegrationFlows
         public string GroupName
         {
             get { return "Integration Flows"; }
+        }
+
+        #endregion
+    }
+
+    internal class KinaxisIntegrationImportFlowPlan : IDesignAddIn, IDesignAddInGuiDetails
+    {
+        #region IDesignAddIn Members
+
+        /// <summary>
+        /// Property returning the name of the add-in. This name may contain any characters and is used as the display name for the add-in in the UI.
+        /// </summary>
+        public string Name
+        {
+            get { return "Import Flow"; }
+        }
+
+        /// <summary>
+        /// Property returning a short description of what the add-in does.
+        /// </summary>
+        public string Description
+        {
+            get { return "Import Flow."; }
+        }
+
+        /// <summary>
+        /// Property returning an icon to display for the add-in in the UI.
+        /// </summary>
+        public System.Drawing.Image Icon
+        {
+            get { return Properties.Resources.ImportFlow; }
+        }
+
+        /// <summary>
+        /// Method called when the add-in is run.
+        /// </summary>
+        public void Execute(IDesignContext context)
+        {
+            // This example code places some new objects from the Standard Library into the active model of the project.
+            if (context.ActiveModel != null)
+            {
+                string importerName = "Web API Importer1";
+                int returnValue = KinaxisIntegrationFlowsUtils.runImportFlowForConnectTablAndResultsTable(context.ActiveModel, importerName, "ConnectToParts", "PartsQueryID", "Parts");
+                if (returnValue == 0) returnValue = KinaxisIntegrationFlowsUtils.runImportFlowForConnectTablAndResultsTable(context.ActiveModel, importerName, "ConnectToParts", "PartsQueryID", "Sites");
+                if (returnValue == 0) returnValue = KinaxisIntegrationFlowsUtils.runImportFlowForConnectTablAndResultsTable(context.ActiveModel, importerName, "ConnectToParts", "PartsQueryID", "PartsSites");
+                if (returnValue == 0) returnValue = KinaxisIntegrationFlowsUtils.runImportFlowForConnectTablAndResultsTable(context.ActiveModel, importerName, "ConnectToBOM", "BOMQueryID", "BOM");
+                if (returnValue == 0) returnValue = KinaxisIntegrationFlowsUtils.runImportFlowForOrdersConnectTablAndResultsTable(context.ActiveModel, importerName, "ConnectToOrders", "DemandOrdersQueryID", "DemandOrders", "SupplyOrdersQueryID", "SupplyOrders", true);
+                if (returnValue == 0) returnValue = KinaxisIntegrationFlowsUtils.runImportFlowForOrdersConnectTablAndResultsTable(context.ActiveModel, importerName, "ConnectToOrders", "DemandOrdersQueryID", "DemandOrderDetails", "SupplyOrdersQueryID", "SupplyOrders", false);
+                if (returnValue == 0) MessageBox.Show("Import Flow Completed", "Success", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        #endregion
+
+        #region IDesignAddInGuiDetails Members
+
+        public string CategoryName
+        {
+            get { return "Planning Table Tools"; }
+        }
+
+        public string TabName
+        {
+            get { return "Content"; }
+        }
+
+        public string GroupName
+        {
+            get { return "Kinaxis"; }
+        }
+
+        #endregion
+    }
+
+    internal class KinaxisIntegrationFlowPlan : IDesignAddIn, IDesignAddInGuiDetails
+    {
+        #region IDesignAddIn Members
+
+        /// <summary>
+        /// Property returning the name of the add-in. This name may contain any characters and is used as the display name for the add-in in the UI.
+        /// </summary>
+        public string Name
+        {
+            get { return "Export Flow"; }
+        }
+
+        /// <summary>
+        /// Property returning a short description of what the add-in does.
+        /// </summary>
+        public string Description
+        {
+            get { return "Export Flow with Plan Resutls."; }
+        }
+
+        /// <summary>
+        /// Property returning an icon to display for the add-in in the UI.
+        /// </summary>
+        public System.Drawing.Image Icon
+        {
+            get { return Properties.Resources.ExportFlow; }
+        }
+
+        /// <summary>
+        /// Method called when the add-in is run.
+        /// </summary>
+        public void Execute(IDesignContext context)
+        {
+            // This example code places some new objects from the Standard Library into the active model of the project.
+            if (context.ActiveModel != null)
+            {
+                string importerName = "Web API Importer1";
+                string exporterName = "CSV Data Exporter1";
+                int returnValue = KinaxisIntegrationFlowsUtils.runExportFlowForConnectTablAndResultsTable(context.ActiveModel, importerName, exporterName, false, "ConnectToParts", "PartsQueryID", "DemandOrderOutputs");
+            }
+        }
+
+        #endregion
+
+        #region IDesignAddInGuiDetails Members
+
+        public string CategoryName
+        {
+            get { return "Planning Table Tools"; }
+        }
+
+        public string TabName
+        {
+            get { return "Content"; }
+        }
+
+        public string GroupName
+        {
+            get { return "Kinaxis"; }
         }
 
         #endregion
